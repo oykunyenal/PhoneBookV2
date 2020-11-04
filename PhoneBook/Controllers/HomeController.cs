08a86adab87a398ecf79d53b7f3bb0801b413ae7
yenal.oykun@outlook.com <yenal.oykun@outlook.com>
2020-11-04 22:05:10 +0300

    AjaxPost ile backend e ulaştık

---
+++ PhoneBook/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Models;
+
+namespace PhoneBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult getFormVal(Contact contactInfo)
+        {
+            String number = contactInfo.Number;
+            return View();
+        }
+
+    }
+}
---
+++ PhoneBook/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models
+{
+    public class Contact
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Number { get; set; }
+        public String Email { get; set; }
+        
+    }
+}
